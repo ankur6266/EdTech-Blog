ae1d627d79ab9bcb51c00d67f7838ed608d51b8a
Ankur Saxena <ankursaxenaankur0@gmail.com>
2022-05-03 20:26:43 +0530

    added register page and some minor changes in login and register buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Pages/Home/Home";
 import Login from "./components/Pages/Login/Login";
+import Register from "./components/Pages/Register/Register";
 import Settings from "./components/Pages/settings/Settings";
 import Single from "./components/Pages/single/Single";
 import Write from "./components/Pages/write/Write";
@@ -15,7 +16,8 @@ function App() {
       {/* <Single /> */}
       {/* <Write /> */}
       {/* <Settings /> */}
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </div>
   )
 }
---
+++ src/components/Pages/Register/Register.jsx
@@ -0,0 +1,21 @@
+import './register.css'
+export default function Register() {
+  return (
+    <div className='register'>
+        
+        <form className='registerForm'>
+        <span className='registerTitle'>Register</span>
+            <label>Username</label>
+            <input className='registerInput' placeholder='Enter Name' />
+            <label>Email</label>
+            <input className='registerInput' type='email' placeholder='Enter Email '/>
+            <label>Password</label>
+            <input className='registerInput' type='password' placeholder='Enter Password '/>
+            <label>Date Of Birth</label>
+            <input className='registerInput' type='date' />
+        <button className='registerButton'>Register</button>
+        </form>
+        <button className='registerLoginButton'>Login ?</button>
+    </div>
+  )
+}
